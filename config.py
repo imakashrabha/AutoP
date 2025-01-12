0a93a0ace03f25f00ef89f8b4575b53120361bef
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-01-12 14:06:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27705761"))
+API_HASH = environ.get("API_HASH", "822cb334ca4527a134aae97f9fe44fd6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7260514430:AAGhXF1Fe8VwS6xri7DERjhOksZGSQs1D9I")
 
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+# Make Bot Admin In Log Channel With Full Rights
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002452631532"))
+ADMINS = int(environ.get("ADMINS", "7246242686"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "autoacceptbot")
+DB_URI = environ.get("DB_URI", "mongodb+srv://akashrabha2005:781120@cluster0.pv6yd2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
